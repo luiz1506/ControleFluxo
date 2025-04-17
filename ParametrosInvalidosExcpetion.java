beee19b33584b4d18afb4642684ba92b56ca0d7d
Luiz G. <66616028+luiz1506@users.noreply.github.com>
2025-04-16 22:19:44 -0300

    Create ParametrosInvalidosExcpetion.java

---
+++ ParametrosInvalidosExcpetion.java
@@ -0,0 +1,8 @@
+ackage com.example.demo.ControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
